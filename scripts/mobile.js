989f5aa06fee4a4d27a0735d17277eb225c47c07
Hutasako <ejjungao@yahoo.com>
2017-01-20 14:57:55 +0400

    New pages and changes
    +Basic view employee page
    +Added alternate "Add project" page

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,11 +1,20 @@
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("navbar");
     if (x.className === "navbar") {
         x.className += " responsive";
     } else {
         x.className = "navbar";
     }
-}
+}*/
 function goBack() {
     window.history.back();
+}
+function myFunction(x) {
+    x.classList.toggle("change");
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
 }
